7c28dab1c88082462234eae22c6892a834da88ac
Henrique Tiago Tyminski <tyminski2011@gmail.com>
2020-08-03 14:10:25 -0300

    PokeApi 01/08/2020

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+    return (
+        <>
+            <div className="Navbar">
+                <span>Pokemon API</span>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
